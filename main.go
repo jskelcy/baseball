ea1ecafceeee5eddfd5ed7794d833ff841baa41a
Jacob Skelcy <jacob.skelcy@liveramp.com>
2019-04-10 10:39:51 -0400

    use api

--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", chrisHandler)
+	tokenPtr := flag.String("token", "foo", "a string")
+	s := server{
+		apiKey: *tokenPtr,
+	}
+	http.HandleFunc("/baseball", s.chrisHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
-func chrisHandler(w http.ResponseWriter, r *http.Request) {
+type server struct {
+	apiKey string
+}
+
+func (s *server) chrisHandler(w http.ResponseWriter, r *http.Request) {
 	leagueStandings := getFantasyChris()
 
-	leagueStandings.populateScores(getMLB())
+	leagueStandings.populateScores(getMLBAPI(s.apiKey))
 	leagueStandings.rank()
 	w.Header().Set("Content-Type", "text/html;charset=UTF-8")
 	w.WriteHeader(http.StatusAccepted)
--- mlb.go
+++ mlb.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
 	"strconv"
 
 	"github.com/gocolly/colly"
@@ -13,7 +17,7 @@ type record struct {
 
 type mlb map[string]record
 
-func getMLB() mlb {
+func getMLBScraped() mlb {
 	t := mlb{}
 
 	c := colly.NewCollector(
@@ -37,4 +41,75 @@ func getMLB() mlb {
 
 	c.Visit("https://erikberg.com/mlb/standings")
 	return t
+}
+
+type mlbStandings struct {
+	StandingsDate string `json:"standings_date"`
+	Standing      []struct {
+		AwayLost                 int     `json:"away_lost"`
+		AwayWon                  int     `json:"away_won"`
+		Conference               string  `json:"conference"`
+		ConferenceLost           int     `json:"conference_lost"`
+		ConferenceWon            int     `json:"conference_won"`
+		Division                 string  `json:"division"`
+		DivisionLost             int     `json:"division_lost"`
+		DivisionWon              int     `json:"division_won"`
+		FirstName                string  `json:"first_name"`
+		GamesBack                float64 `json:"games_back"`
+		GamesPlayed              int     `json:"games_played"`
+		HomeLost                 int     `json:"home_lost"`
+		HomeWon                  int     `json:"home_won"`
+		LastFive                 string  `json:"last_five"`
+		LastName                 string  `json:"last_name"`
+		LastTen                  string  `json:"last_ten"`
+		Lost                     int64   `json:"lost"`
+		OrdinalRank              string  `json:"ordinal_rank"`
+		PointDifferential        int     `json:"point_differential"`
+		PointDifferentialPerGame string  `json:"point_differential_per_game"`
+		PointsAgainst            int     `json:"points_against"`
+		PointsAllowedPerGame     string  `json:"points_allowed_per_game"`
+		PointsFor                int     `json:"points_for"`
+		PointsScoredPerGame      string  `json:"points_scored_per_game"`
+		Rank                     int     `json:"rank"`
+		Streak                   string  `json:"streak"`
+		StreakTotal              int     `json:"streak_total"`
+		StreakType               string  `json:"streak_type"`
+		TeamID                   string  `json:"team_id"`
+		WinPercentage            string  `json:"win_percentage"`
+		Won                      int64   `json:"won"`
+	} `json:"standing"`
+}
+
+func getMLBAPI(token string) mlb {
+	client := &http.Client{}
+	mlbURL, _ := url.Parse("https://erikberg.com/mlb/standings.json")
+	accessHeader := fmt.Sprintf("Bearer %v", token)
+	req := &http.Request{
+		Method: http.MethodGet,
+		URL:    mlbURL,
+		Header: http.Header{
+			"Authorization": []string{accessHeader},
+		},
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("error %v", err)
+	}
+	var rawStandings mlbStandings
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&rawStandings)
+	if err != nil {
+		fmt.Printf("error %v", err)
+	}
+
+	out := mlb{}
+	for _, team := range rawStandings.Standing {
+		out[team.FirstName] = record{
+			wins:   team.Won,
+			losses: team.Lost,
+		}
+	}
+
+	return out
 }
